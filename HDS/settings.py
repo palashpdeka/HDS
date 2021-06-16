5b162febeb858a7015530a49a6cbe124388c8ef8
Palash Pratim Deka <64046049+palashpdeka@users.noreply.github.com>
2021-06-16 13:03:04 +0530

    collectstatic

--- HDS/settings.py
+++ HDS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i7b%m_66u^so#5o0-8v(_h7vzfrblo!bu-%)&^l@o$%-9urr3^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hds1.herokuapp.com']
 
 
 # Application definition
@@ -127,11 +127,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static'),
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')

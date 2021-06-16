7cb4de438f748f737776dfe8f982a5265531af3f
Palash Pratim Deka <64046049+palashpdeka@users.noreply.github.com>
2021-06-16 14:36:25 +0530

    Update settings.py

--- HDS/settings.py
+++ HDS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i7b%m_66u^so#5o0-8v(_h7vzfrblo!bu-%)&^l@o$%-9urr3^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
